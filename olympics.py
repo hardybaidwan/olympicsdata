cd974457a96abc80e399c315d052817b4e803c34
Tajeshwar Singh <67311692+hardybaidwan@users.noreply.github.com>
2021-10-17 10:39:00 +0530

    Update olympics.py

--- olympics.py
+++ olympics.py
@@ -6,7 +6,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.figure_factory as ff
 
-df = pd.read_csv('https://www.kaggle.com/heesoo37/120-years-of-olympic-history-athletes-and-results/athlete_events.csv')
+df = pd.concat(
+    map(pd.read_csv, ['athlete_events.csv', 'athlete_events1.csv']), ignore_index=True)
 region_df = pd.read_csv('noc_regions.csv')
 
 df = preprocessor.preprocess(df,region_df)
